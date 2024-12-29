205f9a18584d2b22bc45e5ad0f731c592902b8ee
Andrei Kuleuski <kuljok@gmail.com>
2024-12-29 19:49:31 +0100

    update Login component styles

--- empoll/src/components/Login.js
+++ empoll/src/components/Login.js
@@ -1,7 +1,39 @@
 import "./Login.css";
 
 const Login = (props) => {
-  return <div className="Login">Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="Login">
+      <picture>Your Logo</picture>
+      <form className="Login-form" onSubmit={handleSubmit}>
+        <section className="title">
+          <h3>Login</h3>
+        </section>
+        <section>
+          <label htmlFor="username">Email</label>
+          <input
+            type="email"
+            id="username"
+            placeholder="username@gmail.com"
+            name="username"
+          />
+        </section>
+        <section>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+          />
+        </section>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
