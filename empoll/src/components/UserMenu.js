f04b7c32f239dc72899ee916bd9036d50c7719ff
Andrei Kuleuski <kuljok@gmail.com>
2025-01-12 17:44:06 +0100

    add toggle class to the logout icon to make it work properly on mobile devices

--- empoll/src/components/UserMenu.js
+++ empoll/src/components/UserMenu.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser";
 import "./UserMenu.css";
 
-const UserMenu = ({ authedUser }) => {
+const UserMenu = ({ dispatch, authedUser }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+  const handleLogout = () => {
+    dispatch(setAuthedUser(null));
+  };
+
   return (
-    <div className="UserMenu">
-      <div className="username">
+    <div className="UserMenu" onClick={handleClick}>
+      <div className={`username ${toggle ? "toggle" : ""}`}>
         <i className="bi bi-person"></i>
         <span>{authedUser.id}</span>
-        <i className="bi bi-x-lg"></i>
+        <i className="bi bi-x-lg" onClick={handleLogout}></i>
       </div>
     </div>
   );
